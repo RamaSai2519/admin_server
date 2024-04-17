330ee99de08dcc54b73f31a4f1285980fb32a823
RamaSai2519 <rchandik@gitam.in>
2024-04-18 01:26:04 +0530

    commit

--- app.py
+++ app.py
@@ -2,18 +2,34 @@
 from flask import Flask, jsonify
 from pymongo import MongoClient
 from flask_cors import CORS
-from pprint import pprint
 
 app = Flask(__name__)
 CORS(app)
 client = MongoClient('mongodb+srv://sukoon_user:Tcks8x7wblpLL9OA@cluster0.o7vywoz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 db = client['test']
 blogs_collection = db['blogposts']
+calls_collection = db['calls']  # Replace 'calls' with your collection name
+
+@app.route('/api/calls')
+def get_calls():
+    calls = list(calls_collection.find({}, {'_id': 0}))
+    for call in calls:
+        call['expert'] = str(call.get('expert', ''))
+        call['user'] = str(call.get('user', ''))
+    return jsonify(calls)
+
+
+@app.route('/api/calls/<string:id>')
+def get_call(id):
+    call = calls_collection.find_one({'callId': id})
+    call['_id'] = str(call['_id'])
+    call['expert'] = str(call.get('expert', ''))
+    call['user'] = str(call.get('user', ''))
+    return jsonify(call)
 
 @app.route('/api/blogs')
 def get_blogs():
     blogs = list(blogs_collection.find({}, {'_id': 0}))
-    pprint(blogs)
     return jsonify(blogs)
 
 @app.route('/api/blogs/<string:id>', methods=['GET'])
@@ -26,7 +42,6 @@ def get_blog(id):
 def get_featured_blog():
     featured_blog = blogs_collection.find_one(sort=[('id', -1)])
     featured_blog['_id'] = str(featured_blog['_id'])
-    pprint(featured_blog)
     return jsonify(featured_blog)
 
 if __name__ == '__main__':
