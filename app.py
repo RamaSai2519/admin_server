03ec2f4c53da690cefa84abe3643a49a7df45477
RamaSai2519 <rchandik@gitam.in>
2024-04-22 11:44:06 +0530

    test

--- app.py
+++ app.py
@@ -96,7 +96,7 @@ def get_last_five_calls():
 
         # If current day's calls are 0, get the latest 5 calls
         if len(current_day_calls) == 0:
-            last_five_calls = list(calls_collection.find().sort([('initiatedTime', -1)]).limit(5))
+            last_five_calls = list(calls_collection.find({'user': {'$nin': excluded_users}}).sort([('initiatedTime', -1)]).limit(5))
         else:
             last_five_calls = current_day_calls
 
--- excluded_users.py
+++ excluded_users.py
@@ -1,5 +1,6 @@
 from bson import ObjectId
 
 excluded_users = [
-    ObjectId("6604618e42f04a057fa20cbb")
+    ObjectId("6604618e42f04a057fa20cbb"),
+    ObjectId("66243796ceab8a1186f0e93c")
 ]
