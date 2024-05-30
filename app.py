6efada21063a81bf2cff3cb1951bcf08074e198b
Rama Sathya Sai <rchandik@gitam.in>
2024-05-30 23:31:31 +0530

    refactor: Update JWT token expiration times

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ Routes from the admin server
         - UserService
         - ExpertService
     - Each service has its own routes
+        - AuthService
+            - /admin/auth/login (tested)
+            - /admin/auth/refresh (tested)
+            - /admin/auth/register (tested)
         - AppService
             - /admin/service/schedule/<string:id> (tested)
             - /admin/service/approve/<string:id>/<level> (tested)
@@ -31,7 +35,7 @@ Routes from the admin server
         - ExpertService
             - /admin/expert/experts/<string:id> (tested)
             - /admin/expert/popupData/<string:expertId> (tested)
- - A total of 17 routes are present in the admin server @ 29/05/2024
+ - A total of 20 routes are present in the admin server @ 30/05/2024
 """
 
 from flask_jwt_extended import JWTManager, jwt_required
@@ -42,6 +46,7 @@ from Utils.Services.CallService import CallService
 from Utils.Services.UserService import UserService
 from Utils.Services.AppService import AppService
 from Utils.config import JWT_SECRET_KEY
+from datetime import timedelta
 from flask_cors import CORS
 from flask import Flask
 
@@ -49,8 +54,8 @@ app = Flask(__name__)
 CORS(app, supports_credentials=True)
 
 app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = 900
-app.config["JWT_REFRESH_TOKEN_EXPIRES"] = 2592000
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
+app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=1)
 jwt = JWTManager(app)
 
 
@@ -74,37 +79,44 @@ def register_route():
 
 # Below are the functional routes, prefixed with /service
 @app.route("/admin/service/schedule/<id>", methods=["PUT", "DELETE", "GET"])
+@jwt_required()
 def update_schedule_route(id):
     return AppService.update_schedule(id)
 
 
 @app.route("/admin/service/approve/<string:id>/<level>", methods=["PUT"])
+@jwt_required()
 def approve_user(id, level):
     return AppService.approve_application(id, level)
 
 
 @app.route("/admin/service/save-fcm-token", methods=["POST"])
+@jwt_required()
 def save_fcm_token_route():
     return AppService.save_fcm_token()
 
 
 @app.route("/admin/service/dashboardstats")
+@jwt_required()
 def get_dashboard_stats_route():
     return AppService.get_dashboard_stats()
 
 
 # Below are the CallService routes, prefixed with /call
 @app.route("/admin/call/calls/<string:id>", methods=["GET", "PUT"])
+@jwt_required()
 def handle_call_route(id):
     return CallService.handle_call(id)
 
 
 @app.route("/admin/call/callUser", methods=["POST"])
+@jwt_required()
 def expert_call_user_route():
     return CallService.expert_call_user()
 
 
 @app.route("/admin/call/connect", methods=["POST"])
+@jwt_required()
 def connect_route():
     return CallService.connect()
 
@@ -117,56 +129,67 @@ def get_error_logs_route():
 
 
 @app.route("/admin/data/calls")
+@jwt_required()
 def get_calls_route():
     return DataService.get_all_calls()
 
 
 @app.route("/admin/data/applications")
+@jwt_required()
 def get_applications_route():
     return DataService.get_applications()
 
 
 @app.route("/admin/data/users")
+@jwt_required()
 def get_users_route():
     return DataService.get_users()
 
 
 @app.route("/admin/data/experts")
+@jwt_required()
 def get_experts_route():
     return DataService.get_experts()
 
 
 @app.route("/admin/data/categories")
+@jwt_required()
 def get_categories_route():
     return DataService.get_categories()
 
 
 @app.route("/admin/data/schedules", methods=["POST", "GET"])
+@jwt_required()
 def schedules_route():
     return DataService.schedules()
 
 
 # Below are the ExpertService routes, prefixed with /expert
 @app.route("/admin/expert/experts/<string:id>", methods=["GET", "PUT", "DELETE"])
+@jwt_required()
 def handle_expert_route(id):
     return ExpertService.handle_expert(id)
 
 
 @app.route("/admin/expert/popupData/<string:expertId>", methods=["GET"])
+@jwt_required()
 def get_popup_data_route(expertId):
     return ExpertService.get_popup_data(expertId)
 
 
 # Below are the UserService routes, prefixed with /user
 @app.route("/admin/user/leads", methods=["GET"])
+@jwt_required()
 def get_leads_route():
     return UserService.get_leads()
 
 
 @app.route("/admin/user/users/<string:id>", methods=["GET", "PUT", "DELETE"])
+@jwt_required()
 def handle_user_route(id):
     return UserService.handle_user(id)
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
+    
