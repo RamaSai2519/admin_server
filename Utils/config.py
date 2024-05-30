67a0ecb258b16be79dc6549933f44a4b4496e6a5
Rama Sathya Sai <rchandik@gitam.in>
2024-05-30 21:22:41 +0530

    refactor: Update database connection URL and firebase_admin.initialize_app() path

--- Utils/config.py
+++ Utils/config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 import firebase_admin
 import os
 
-firebase_admin.initialize_app(credentials.Certificate("serviceAccountKey.json"))
 load_dotenv()
 
 JWT_SECRET_KEY = os.getenv("JWT_SECRET")
+firebase_admin.initialize_app(credentials.Certificate("serviceAccountKey.json"))
 
 client = MongoClient(os.getenv("PROD_DB_URL"))
 db = client["test"]
