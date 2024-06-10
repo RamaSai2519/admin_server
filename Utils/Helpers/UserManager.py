0c8673397441f3ef9a1f817468110b3bf599a1bf
Rama Sathya Sai <rchandik@gitam.in>
2024-06-10 16:45:19 +0530

    Fix: UserManager @ 52

--- Utils/Helpers/UserManager.py
+++ Utils/Helpers/UserManager.py
@@ -24,41 +24,42 @@ class UserManager:
 
     @staticmethod
     def get_user_context(user_id):
-        if meta_collection.find_one({"user": user_id}) is None:
-            return "User context not found."
-        else:
-            try:
-                if users_collection.find_one({"_id": user_id}) is not None:
-                    user = users_collection.find_one(
-                        {"_id": user_id}, {"_id": 0, "phoneNumber": 0}
-                    )
-                    user_city = user["city"]
-                    user_dob = user["birthDate"]
-                    user_name = user["name"]
-                    user_age = datetime.now().year - user_dob.year
-                    personal_info = {
-                        "name": user_name if "name" in user else "Unknown",
-                        "city": user_city if "city" in user else "Unknown",
-                        "age": user_age if "birthDate" in user else "Unknown",
-                    }
-                    user_persona = (
-                        user["Customer Persona"]
-                        if "Customer Persona" in user
-                        else "Unknown"
-                    )
+        try:
+            if users_collection.find_one({"_id": user_id}) is not None:
+                user = users_collection.find_one(
+                    {"_id": user_id}, {"_id": 0, "phoneNumber": 0}
+                )
+                user_city = user["city"]
+                user_dob = user["birthDate"]
+                user_name = user["name"]
+                user_age = datetime.now().year - user_dob.year
+                personal_info = {
+                    "name": user_name if "name" in user else "Unknown",
+                    "city": user_city if "city" in user else "Unknown",
+                    "age": user_age if "birthDate" in user else "Unknown",
+                }
+                user_persona = (
+                    user["Customer Persona"]
+                    if "Customer Persona" in user
+                    else "Unknown"
+                )
+                try:
                     document = meta_collection.find_one({"user": user_id})
                     if "context" in document:
                         context = str(document["context"])
                         context = context.split("\n") if context != "" else "Unknown"
-                    return {
-                        "personal_info": personal_info,
-                        "context": context,
-                        "persona": user_persona,
-                    }
-                else:
-                    return "No user found."
-            except Exception as e:
-                return "Something Went Wrong."
+                except Exception as e:
+                    print(e, "@52")
+                    context = "Unkown"
+                return {
+                    "personal_info": personal_info,
+                    "context": context,
+                    "persona": user_persona,
+                }
+            else:
+                return "No user found."
+        except Exception as e:
+            return "Something Went Wrong."
 
     @staticmethod
     def determine_user_repeation(user_id, expert_id):
