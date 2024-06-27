b47701da8148264565d698276b3eaf198e2b75c1
Rama Sathya Sai <rchandik@gitam.in>
2024-06-27 21:34:46 +0530

    refactor: Improve get_user_context method in UserManager
    The code changes in `UserManager.py` refactor the `get_user_context` method to improve its efficiency and readability. The changes include simplifying the conditional statements and removing unnecessary try-except blocks. This update enhances the `UserManager` class by making the code more concise and easier to maintain.

--- Utils/Helpers/UserManager.py
+++ Utils/Helpers/UserManager.py
@@ -24,48 +24,45 @@ class UserManager:
 
     @staticmethod
     def get_user_context(user_id):
-        try:
-            if users_collection.find_one({"_id": user_id}) is not None:
-                user = users_collection.find_one(
-                    {"_id": user_id}, {"_id": 0, "phoneNumber": 0}
-                )
-                user_city = user["city"]
-                user_dob = user["birthDate"]
-                user_name = user["name"]
-                user_age = datetime.now().year - user_dob.year
-                personal_info = {
-                    "name": user_name if "name" in user else "Unknown",
-                    "city": user_city if "city" in user else "Unknown",
-                    "age": user_age if "birthDate" in user else "Unknown",
-                }
-                user_persona = (
-                    user["Customer Persona"]
-                    if "Customer Persona" in user
-                    else "Unknown"
-                )
-                try:
-                    document = meta_collection.find_one({"user": user_id})
-                    if "context" in document:
-                        context = str(document["context"])
-                        context = context.split("\n") if context != "" else "Unknown"
-                except Exception as e:
-                    print(e, "@52")
-                    context = []
-                return {
-                    "personal_info": personal_info,
-                    "context": context,
-                    "persona": user_persona,
-                }
+        if users_collection.find_one({"_id": user_id}) is not None:
+            user = users_collection.find_one(
+                {"_id": user_id}, {"_id": 0, "phoneNumber": 0}
+            )
+            user_city = user["city"]
+            user_dob = user["birthDate"]
+            user_name = user["name"]
+            user_age = datetime.now().year - user_dob.year
+            personal_info = {
+                "name": user_name if "name" in user else "Unknown",
+                "city": user_city if "city" in user else "Unknown",
+                "age": user_age if "birthDate" in user else "Unknown",
+            }
+            user_persona = (
+                user["Customer Persona"]
+                if "Customer Persona" in user
+                else "Unknown"
+            )
+            document = meta_collection.find_one({"user": user_id})
+            if "context" in document:
+                context = str(document["context"])
+                context = context.split(
+                    "\n") if context != "" else "Unknown"
             else:
-                return "No user found."
-        except Exception as e:
-            return "Something Went Wrong."
+                context = [""]
+            return {
+                "personal_info": personal_info,
+                "context": context,
+                "persona": user_persona,
+            }
+        else:
+            return "No user found."
 
     @staticmethod
     def determine_user_repeation(user_id, expert_id):
         if len(list(calls_collection.find({"user": user_id}))) > 1:
             if (
-                len(list(calls_collection.find({"user": user_id, "expert": expert_id})))
+                len(list(calls_collection.find(
+                    {"user": user_id, "expert": expert_id})))
                 > 1
             ):
                 return "You have connected to this user before."
