6757e843298c5c43fcb1bd91f72ca9732a1333c2
Rama Sathya Sai <rchandik@gitam.in>
2024-06-28 11:35:29 +0530

    refactor: Add pagination to DataService schedules GET request
    The code changes in `DataService.py` modify the `schedules` method in the `DataService` class. The GET request for schedules now includes pagination, allowing the retrieval of schedules based on the requested page and size. This change improves the efficiency of the API response by reducing unnecessary data retrieval and processing.

--- Utils/Services/DataService.py
+++ Utils/Services/DataService.py
@@ -86,14 +86,24 @@ class DataService:
     @staticmethod
     def schedules():
         if request.method == "GET":
+            page = int(request.args.get('page'))
+            size = int(request.args.get('size'))
+            offset = (page - 1) * size
+
             schedules = list(schedules_collection.find({}, {
                 "lastModifiedBy": 0
-            }).sort("datetime", 1))
+            }).sort("datetime", -1).skip(offset).limit(size))
+
+            total_schedules = schedules_collection.count_documents({})
+
             for schedule in schedules:
                 schedule["_id"] = str(schedule["_id"])
                 schedule["expert"] = hf.get_expert_name(schedule["expert"])
                 schedule["user"] = hf.get_user_name(schedule["user"])
-            return jsonify(schedules)
+            return jsonify({
+                "data": schedules,
+                "total": total_schedules
+            })
         elif request.method == "POST":
             data = request.json
             user_id = data["user"]
