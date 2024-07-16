19f801a8cce52f77710a409bb48cc42e8ebe6112
Rama Sathya Sai <rchandik@gitam.in>
2024-07-16 23:36:43 +0530

    refactor: Update DataService sort field to "createdDate"

--- Utils/Services/DataService.py
+++ Utils/Services/DataService.py
@@ -1,54 +1,40 @@
 from Utils.config import (
-    userwebhookmessages_collection,
     applications_collection,
-    wafeedback_collection,
     categories_collection,
     errorlogs_collection,
-    schedules_collection,
     experts_collection,
     timings_collection,
     users_collection,
     indianLanguages
 )
-from Utils.Helpers.UtilityFunctions import UtilityFunctions as uf
-from Utils.Helpers.HelperFunctions import HelperFunctions as hf
-from Utils.Helpers.ScheduleManager import ScheduleManager as sm
-from Utils.Helpers.FormatManager import FormatManager as fm
-from Utils.Helpers.AuthManager import AuthManager as am
-from datetime import datetime, timedelta
+from Utils.Helpers.UtilityFunctions import UtilityFunctions
+from Utils.Helpers.HelperFunctions import HelperFunctions
+from Utils.Helpers.FormatManager import FormatManager
 from flask import jsonify, request
+from datetime import datetime
 from bson import ObjectId
-import pytz
 
 
 class DataService:
-    """
-    A class that provides static methods to retrieve data.
-    - get_error_logs(): Retrieves error logs from a collection, converts the '_id' field to a string, and returns the logs as JSON.
-    - get_applications(): Processes feedback data by getting the expert name, modifying the body text, and returns the feedbacks data along with total count, page, and page size as JSON.
-    - get_all_calls(): Retrieves all calls data from a collection and returns the calls data as JSON.
-    - get_experts(): Retrieves experts data from a collection, formats the data, and returns the experts data as JSON.
-    - get_users(): Retrieves users data from a collection, modifies the '_id', 'lastModifiedBy', 'createdDate', and 'userGameStats' fields, and returns the users data as JSON.
-    - get_categories(): Retrieves categories data from a collection, extracts the 'name' field, and returns the category names as JSON.
-    - get_timings(): Retrieves timings data from a collection, processes the data, and returns the timings data as JSON.
-    """
-    @staticmethod
-    def get_error_logs():
-        size, offset, page = uf.pagination_helper()
+    def __init__(self):
+        self.uf = UtilityFunctions
+        self.hf = HelperFunctions
+        self.fm = FormatManager
+
+    def get_error_logs(self):
+        size, offset, page = self.uf.pagination_helper()
         error_logs = list(errorlogs_collection.find().sort(
             "time", -1).skip(offset).limit(size))
-        for error_log in error_logs:
-            error_log["_id"] = str(error_log["_id"])
+        error_logs = list(
+            map(self.hf.convert_objectids_to_strings, error_logs))
         total_error_logs = errorlogs_collection.count_documents({})
         return jsonify({"data": error_logs, "total": total_error_logs})
 
-    @staticmethod
-    def get_applications():
+    def get_applications(self):
         formType = request.args.get('formType', 'sarathi')
-        size, offset, page = uf.pagination_helper()
+        size, offset, page = self.uf.pagination_helper()
         applications = list(applications_collection.find(
-            {"formType": formType}
-        ).sort("createdDate", -1).skip(offset).limit(size))
+            {"formType": formType}).sort("createdDate", -1).skip(offset).limit(size))
 
         for application in applications:
             application["_id"] = str(application["_id"])
@@ -56,53 +42,37 @@ class DataService:
                 "'", "").replace("[", "").replace("]", "") if "workingHours" in application else ""
             if "languages" in application:
                 languages = application["languages"]
-                final_languages = []
-                for language in languages:
-                    for indianLanguage in indianLanguages:
-                        if indianLanguage["key"] == language:
-                            language = indianLanguage["value"]
-                            final_languages.append(language)
+                final_languages = [indianLanguage["value"]
+                                   for language in languages for indianLanguage in indianLanguages if indianLanguage["key"] == language]
                 application["languages"] = str(final_languages).replace(
                     "'", "").replace("[", "").replace("]", "")
+
         total_applications = applications_collection.count_documents(
             {"formType": formType})
-        return jsonify({
-            "data": applications,
-            "total": total_applications
-        })
+        return jsonify({"data": applications, "total": total_applications})
 
-    @staticmethod
-    def get_all_calls():
-        return uf.get_calls()
+    def get_all_calls(self):
+        return self.uf.get_calls()
 
-    @staticmethod
-    def get_experts():
+    def get_experts(self):
         experts = list(experts_collection.find(
             {}, {"categories": 0}).sort("name", 1))
-        formatted_experts = [fm.get_formatted_expert(
+        formatted_experts = [self.fm.get_formatted_expert(
             expert) for expert in experts]
         return jsonify(formatted_experts)
 
-    @staticmethod
-    def get_users():
-        users = list(
-            users_collection.find(
-                {"role": {"$ne": "admin"}},
-                {"Customer Persona": 0},
-            )
-        )
+    def get_users(self):
+        users = list(users_collection.find(
+            {"role": {"$ne": "admin"}}, {"Customer Persona": 0}))
         for user in users:
             user["_id"] = str(user["_id"])
-            user["lastModifiedBy"] = (
-                str(user["lastModifiedBy"]) if "lastModifiedBy" in user else ""
-            )
-            user["userGameStats"] = (
-                str(user["userGameStats"]) if "userGameStats" in user else ""
-            )
+            user["lastModifiedBy"] = str(
+                user["lastModifiedBy"]) if "lastModifiedBy" in user else ""
+            user["userGameStats"] = str(
+                user["userGameStats"]) if "userGameStats" in user else ""
         return jsonify(users)
 
-    @staticmethod
-    def get_categories():
+    def get_categories(self):
         if request.method == "GET":
             categories = list(categories_collection.find(
                 {}, {"_id": 0, "name": 1}))
@@ -115,17 +85,14 @@ class DataService:
             category = data["name"]
             createdDate = datetime.now()
             categories_collection.insert_one(
-                {"name": category, "createdDate": createdDate, "active": True}
-            )
+                {"name": category, "createdDate": createdDate, "active": True})
             return jsonify({"message": "Category added successfully"})
 
-    @staticmethod
-    def get_timings():
+    def get_timings(self):
         if request.method == "GET":
             expertId = request.args.get("expert")
-            timings = list(timings_collection.find({
-                "expert": ObjectId(expertId)
-            }))
+            timings = list(timings_collection.find(
+                {"expert": ObjectId(expertId)}))
             for timing in timings:
                 timing["_id"] = str(timing["_id"])
                 timing["expert"] = str(timing["expert"])
@@ -138,19 +105,13 @@ class DataService:
             field = data["row"]["field"]
             value = data["row"]["value"]
 
-            fields = [
-                "PrimaryStartTime",
-                "PrimaryEndTime",
-                "SecondaryStartTime",
-                "SecondaryEndTime",
-            ]
+            fields = ["PrimaryStartTime", "PrimaryEndTime",
+                      "SecondaryStartTime", "SecondaryEndTime"]
             if field not in fields:
                 return jsonify({"error": "Invalid field"}), 400
             try:
-                timing = timings_collection.find_one({
-                    "expert": ObjectId(expertId)
-                })
-
+                timing = timings_collection.find_one(
+                    {"expert": ObjectId(expertId)})
                 if timing:
                     timings_collection.update_one({"expert": ObjectId(expertId)}, {
                                                   "$set": {field: value}})
--- routes/data_routes.py
+++ routes/data_routes.py
@@ -4,44 +4,46 @@ from flask import Blueprint
 
 data_routes = Blueprint("data_routes", __name__)
 
+data_service = DataService()
+
 
 @data_routes.route("/admin/data/errorlogs")
 @jwt_required()
 def get_error_logs_route():
-    return DataService.get_error_logs()
+    return data_service.get_error_logs()
 
 
 @data_routes.route("/admin/data/calls")
 @jwt_required()
 def get_calls_route():
-    return DataService.get_all_calls()
+    return data_service.get_all_calls()
 
 
 @data_routes.route("/admin/data/applications")
 @jwt_required()
 def get_applications_route():
-    return DataService.get_applications()
+    return data_service.get_applications()
 
 
 @data_routes.route("/admin/data/users")
 @jwt_required()
 def get_users_route():
-    return DataService.get_users()
+    return data_service.get_users()
 
 
 @data_routes.route("/admin/data/experts")
 @jwt_required()
 def get_experts_route():
-    return DataService.get_experts()
+    return data_service.get_experts()
 
 
 @data_routes.route("/admin/data/categories", methods=["GET", "POST"])
 @jwt_required()
 def get_categories_route():
-    return DataService.get_categories()
+    return data_service.get_categories()
 
 
 @data_routes.route("/admin/data/timings", methods=["GET", "POST"])
 @jwt_required()
 def timings_route():
-    return DataService.get_timings()
+    return data_service.get_timings()
