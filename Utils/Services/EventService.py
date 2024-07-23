1155e7b45a08abea2d44248789bac18e2fdf4474
Rama Sathya Sai <140180940+RamaSai2519@users.noreply.github.com>
2024-07-23 10:49:39 +0530

    Update EventService.py

--- Utils/Services/EventService.py
+++ Utils/Services/EventService.py
@@ -91,7 +91,7 @@ class EventService:
                     return jsonify({"error": "Slug already exists"}), 400
                 else:
                     url = f"{MAIN_BE_URL}/events/config"
-                    response = requests.post(url, data=data)
+                    response = requests.request("POST", url, data=data)
                     return response.json()
             except Exception as e:
                 print(e)
